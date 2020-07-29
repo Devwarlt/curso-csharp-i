b94040ec223d0d7ad9b51edc4099989061e3f79b
Devwarlt <devwarlt@loesoft.org>
2020-07-29 08:41:38 -0300

    Aula 6 desafio
    Falha durante a configuração do EF6.

---
+++ C6Challenge/Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace C6Challenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ C6Challenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace C6Challenge
+{
+    /* Aqui, você deve criar um projeto no Visual Studio com o nome Cadastro de Clientes.
+     *
+     * Neste projeto, você deve adicionar uma nova fonte de dados como Arquivo do Microsoft
+     * SQL Server e instalar o Entity Framework.
+     *
+     * Você deve anexar o projeto e também imagens da tela que mostrem o Gerenciador de
+     * Servidores com o banco de dados e, no Gerenciador de Soluções, a exibição da Referência
+     * do Entity Framework no projeto. */
+
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
