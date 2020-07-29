7009aed1ea390fe9071d62ede09b39960e1b7052
Devwarlt <devwarlt@loesoft.org>
2020-07-29 07:18:16 -0300

    Aula 4 desafio

---
+++ C4Challenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace C4Challenge
+{
+    /* Após a instalação você deve iniciar um projeto do tipo Windows Forms com Visual C#.
+     * O nome do projeto deve ser "ProjetoDesafio123". Você não pode desenvolver ou modificar
+     * o projeto inicial, apenas criá-lo. */
+
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
